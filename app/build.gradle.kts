9e88cb80af6a1adaa24d08e31e9882987592c2f3
AmrAmin19 <100246329+AmrAmin19@users.noreply.github.com>
2024-10-05 23:20:56 +0300

    chips added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation ("com.google.android.material:material:1.9.0")
 
+
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
 
