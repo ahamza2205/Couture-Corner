9f2b3cf7ac4219b3faf4f1cf78fca991d9c8ad91
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2024-10-07 11:46:15 +0300

    handle sign up bugs

--- app/src/main/java/com/example/couturecorner/setting/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/couturecorner/setting/ui/SettingsFragment.kt
@@ -1,20 +1,12 @@
 package com.example.couturecorner.setting.ui
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.content.Intent
-import android.content.SharedPreferences
 import com.example.couturecorner.R
-import com.example.couturecorner.authentication.view.LoginActivity
-
 class SettingsFragment : Fragment() {
-
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,22 +14,6 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        sharedPreferences = requireActivity().getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
-
-        val logoutButton: Button = view.findViewById(R.id.button)
-        logoutButton.setOnClickListener {
-            logoutUser()
-        }
-
         return view
-    }
-
-    private fun logoutUser() {
-        sharedPreferences.edit().remove("isLoggedIn").apply()
-
-        val intent = Intent(activity, LoginActivity::class.java)
-        startActivity(intent)
-        activity?.finish()
     }
 }
